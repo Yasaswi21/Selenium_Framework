12016e4355bc79a9aee31c22b02bc586186835b0
Yasaswi21 <108977088+Yasaswi21@users.noreply.github.com>
2025-07-10 11:27:21 +0530

    Update DeletePage.java

--- src/main/java/Pages/DeletePage.java
+++ src/main/java/Pages/DeletePage.java
@@ -11,23 +11,32 @@ import Utils.ObjectRepoReader;
 
 public class DeletePage {
 
-	WebDriver driver;
-	UIActions ui;
+    WebDriver driver;
+    UIActions ui;
 
-	// Locators
-	By deleteField = ObjectRepoReader.getLocator("DeletePage", "deleteField");
-	By confirmation = ObjectRepoReader.getLocator("DeletePage", "confirmation");
-	
-	//Constructor
-	public DeletePage(WebDriver driver) {
-		this.driver = driver;
-		this.ui = new UIActions(driver);
-	}
-	
-	public void del(ExtentTest test) throws InterruptedException {
-		ui.click(deleteField, "Clicking delete", test);
-		ui.click(confirmation, "Confirming To Delete User", test);
-		Thread.sleep(2000);
-		DeleteUserValidator.validateDeleteUser(driver, test);
-	}
+    // Locators
+    By deleteField = ObjectRepoReader.getLocator("DeletePage", "deleteField");
+    By confirmation = ObjectRepoReader.getLocator("DeletePage", "confirmation");
+
+    // Constructor
+    public DeletePage(WebDriver driver) {
+        this.driver = driver;
+        this.ui = new UIActions(driver);
+    }
+
+    // Actions
+    public boolean del(ExtentTest test) {
+        try {
+            ui.click(deleteField, "Clicking delete", test);
+            ui.click(confirmation, "Confirming To Delete User", test);
+            
+
+            boolean isDeleted = DeleteUserValidator.validateDeleteUser(driver, test);
+            return isDeleted;
+
+        } catch (Exception e) {
+            test.fail("Delete User process failed due to: " + e.getMessage());
+            return false;
+        }
+    }
 }
